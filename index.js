bf2d3aaafbe5529a58cd7fae5fc0412d55525823
khatere-hn <78313794+khatere-hn@users.noreply.github.com>
2021-10-30 20:24:37 +0330

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ function q1(){
     elemnt.classList.add('js_p');
     q[0].appendChild(elemnt);
 }
+function answer(a ,i,event){
+    let p = document.createElement('p') ;
+    p.textContent = a ;
+    p.classList.add('float_left') ;
+    p.style.fontSize = '2rem' ;
+    btn[i].before(p);
+}
+
+for (let i = 1; i < btn.length ; i++) {
+    btn[i].addEventListener('click',answer.bind(this , btn[i].dataset.answer , i)) ;
+}
